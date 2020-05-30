3297093a7e15a5336a9c0b1f421b409489e91b25
Alexander Lomonosov <skilldill@yandex.ru>
2020-05-30 15:54:51 +0500

    add 404

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,19 +1,41 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
+import cn from "classnames";
 
 import "./style.scss";
 import { NavbarActions } from "../../store/navbar/navbar.actions";
+import Kolyan from "../../assets/Колян.png";
 
 export const NotFound = () => {
+    const [showKolyan, setShowKolyan] = useState(false);
     const dispatch = useDispatch();
     
     useEffect(() => {
         dispatch(NavbarActions.setTitle('Пууустооо...'))
+        const timerKolyan = setTimeout(() => {
+            setShowKolyan(true);
+        }, 100);
+
+        return () => {
+            clearTimeout(timerKolyan);
+        }
     }, []);
+
+    const classesKolyan = cn({
+        'kolyan': true,
+        'kolyan-show': showKolyan
+    })
 
     return (
         <div className="not-found">
-            404
+            <h1>
+                Ты куда своим пальцем тычешь?
+            </h1>
+            <img 
+                className={classesKolyan} 
+                src={Kolyan} 
+                alt="kolyan"
+            />
         </div>
     )
 }
