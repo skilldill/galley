faad2776679a829be34a10492d1a128aa1442a03
Alexander Lomonosov <skilldill@yandex.ru>
2020-05-31 01:29:59 +0500

    add charts

--- src/pages/Rating/Rating.jsx
+++ src/pages/Rating/Rating.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Chart } from "react-charts";
 
 import "./style.scss";
 import { NavbarActions } from "../../store/navbar/navbar.actions";
@@ -7,15 +8,28 @@ import { ChartActions } from "../../store/chart/chart.actions";
 
 export const Rating = () => {
     const dispatch = useDispatch();
-    
+    const { data } = useSelector((state) => state.chart);
+
     useEffect(() => {
         dispatch(NavbarActions.setTitle('Показатель гребли'));
-        dispatch(ChartActions.fetchChartData(10));
+        dispatch(ChartActions.fetchChartData(100));
     }, []);
+
+    const axes = [
+        { primary: true, type: 'linear', position: 'bottom' },
+        { type: 'linear', position: 'left' },
+    ]
 
     return (
         <div className="rating">
-            Показатель гребли
+           
+            { 
+                !!data.length && (
+                    <div className="chart-holder">
+                        <Chart data={data} axes={axes} tooltip />
+                    </div>
+                )
+            }
         </div>
     )
 }
--- src/store/chart/chart.reducer.js
+++ src/store/chart/chart.reducer.js
@@ -6,11 +6,43 @@ const initialState = {
     count: 10
 }
 
-const mapReducer = {
-    [CHART_ACTION_TYPES.SET_DATA]: (state, action) => ({
-        ...state,
-        data: [...state.data, ...action.payload]
+const prepareData = (data) => {
+    const uniqueByHash = [...new Set(data.map((dot) => dot.hash))];
+    const updateDataArray = [];
+
+    uniqueByHash.map((hash) => {
+        // Создаем массив уникальных массивов
+        const uniqueArr = data.filter((dot) => dot.hash === hash);
+        updateDataArray.push(uniqueArr);
     })
+
+    const readyObjects = [];
+
+    updateDataArray.map((arr) => {
+        const obj = {
+            label: arr[0].hash,
+            data: []
+        }
+
+        arr.reverse().map((dot, i) => {
+            obj.data.push([i, dot.count])
+        });
+
+        readyObjects.push(obj);
+    })
+
+    return readyObjects;
+}
+
+const mapReducer = {
+    [CHART_ACTION_TYPES.SET_DATA]: (state, action) => {
+        const data = prepareData(action.payload);
+
+        return {
+            ...state,
+            data: data.reverse()
+        }
+    }
 }
 
 export const chartReducer = handleActions(mapReducer, initialState);
