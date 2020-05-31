0cdaae32464c6e0e84e5efc591f51c219a016336
Alexander Lomonosov <skilldill@yandex.ru>
2020-05-31 17:37:53 +0500

    add description

--- src/pages/Users/components/User/User.jsx
+++ src/pages/Users/components/User/User.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./style.scss";
 import firtsFacePng from "../../../../assets/4.png";
@@ -6,17 +6,36 @@ import secondFacePng from "../../../../assets/2.png";
 import thrithFacePng from "../../../../assets/6.png";
 
 export const User = (props) => {
+    const [content, setContent] = useState('');
     const { name, secondName, index } = props;
+
+    useEffect(() => {
+        if ((index % 2) === 0) {
+            setContent(`Рождённый в пригороде Стагира, с детства выделялся своими способностями к красноречию и гребле. В настоящее же время усердно гребет с остальными коллегами.`);
+            return;
+        }
+
+        if ((index % 3) === 0) {
+            setContent(`Родом из Кенхреи, славен своими трудами в изобразительном искувстве, и инженерном деле. Сейчас на нем держится весь внешний вид Galley`);
+            return;
+        }
+        
+        setContent(`Ее мы подобрали у берегов Аль-Мина, ничем не примечательна, кроме уродливого внешнего вида.`);
+        return;
+    }, [])
 
     const renderAvatar = () => {
         if ((index % 2) === 0) {
+            // setContent(`Рождённый в пригороде Стагира, с детства выделялся своими способностями к красноречию и гребле. В настоящее же время усердно гребет с остальными коллегами.`);
             return <img src={firtsFacePng} alt="avatar"/>
         }
 
         if ((index % 3) === 0) {
+            // setContent(`Родом из Кенхреи, славен своими трудами в изобразительном искувстве, и инженерном деле. Сейчас на нем держится весь внешний вид Galley`);
             return <img src={secondFacePng} alt="avatar"/>
         }
-
+        
+        // setContent(`Ее мы подобрали у берегов Аль-Мина, ничем не примечательна, кроме уродливого внешнего вида.`);
         return <img src={thrithFacePng} alt="avatar"/>
     }
 
@@ -26,8 +45,8 @@ export const User = (props) => {
                 {renderAvatar()}
             </div>
             <div className="info">
-                <h3>{name}</h3>
-                <h3>{secondName}</h3>
+                <h3>{`${name} ${secondName}`}</h3>
+                <p>{content}</p>
             </div>
         </div>
     )
